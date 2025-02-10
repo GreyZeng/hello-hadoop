101d8d376f50645a4ef506a45e60a1da011cc616
GreyZeng <410486047@qq.com>
2025-02-10 14:19:10 +0800

    update code

---
+++ src/main/java/git/snippets/mr/withcombiner/WordCountMapper.java
@@ -0,0 +1,36 @@
+package git.snippets.mr.withcombiner;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * Mapper
+ * 自定义类继承Mapper抽象类，实现map方法
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+
+    Text returnKey = new Text();
+    IntWritable returnValue = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        //hello zhangsan
+        String line = value.toString();
+
+        //切分单词
+        String[] words = line.split(" ");
+
+        //写出数据
+        for (String word : words) {
+            returnKey.set(word);
+            //k,v : word,1
+            context.write(returnKey,returnValue);
+        }
+
+
+    }
+}
